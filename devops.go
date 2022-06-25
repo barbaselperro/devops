27c6e640a43bc41302927dabcb76900ae9d488b6
barbaselperro <54598818+barbaselperro@users.noreply.github.com>
2022-06-24 23:19:31 -0300

    Create devops.go

---
+++ devops.go
@@ -0,0 +1,19 @@
+package main
+
+/* vas con tus amigos s un restaurant y acuerdan dividir la cuenta, para facilitar las 
+cosas pediras al usuario que indique el total a pagar, entre cuantas personas se dividira 
+la cuenta, %de propina a incluir, un porcentaje de impuesto, total a pagar incluyendo 
+propina mas impouesto y el total a pagar por cada persona  */
+
+import "fmt"
+func main(){
+	var pagoTotal , personas, propina, impuesto int
+	fmt.Println("ingrese el pago total de la cuenta: $")
+	fmt.Scan(&pagoTotal)
+	fmt.Print("ingrese en cuantas personas se divide la cuenta: ")
+	fmt.Scan(&personas)
+	fmt.Print("ingrese un porcentaje de propina: %")
+	fmt.Scan(&propina)
+	//falta continuar
+
+}
